7272dfa187380b7a27f3c010fdcb11031eb4071d
Thiago Nunes <thiagotnunes@gmail.com>
2011-08-16 19:57:42 -0300

    Fixing close button

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,16 +1,18 @@
 $(document).ready(function() {
-    $('#new-space').click(addStoryCard);
-
-    function addStoryCard() {
-        var card = storyCard('#', '')
-        $('#black-box').show();
-        card.html().css('z-index', 1).appendTo($('#content')).fadeIn('slow');
-    }
-    $('#new-space').click();
+    var blackBox = $('#black-box');
+    var card = storyCard('#', 'blahhhh').html().css('z-index', 1).appendTo($('#content'));
     $('#close-card').click(closeCard);
 
+    $('#new-space').click(showStoryCard);
+
+    function showStoryCard() {
+        blackBox.show();
+        card.fadeIn('slow');
+    }
+
+    $('#new-space').click();
+    
     function closeCard() {
-        var blackBox = $('#black-box');
         if(blackBox.is(':visible'))
             blackBox.hide();
         $(this).parent().hide();
